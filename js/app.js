7af347fabaea5cada7f4c7a91f0e9bd2403cd189
Palafox <israel_fox3@hotmail.com>
2022-10-04 13:48:39 -0500

    04/10/2022, ejemplo de uso jsonplaceholder

--- js/app.js
+++ js/app.js
@@ -7,4 +7,8 @@ $(document).ready(function (){
 		var year2=$("#year2").val();
 		$("# year").append('<option value="'+year2+'">'+year2+'</option>');
 	});
+
+	$.get( "https://jsonplaceholder.typicode.com/posts", function( data ) {
+		  console.log( data );
+		});
 });
